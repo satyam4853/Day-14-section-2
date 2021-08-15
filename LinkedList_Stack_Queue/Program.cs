399658012b1a48d94c56475fe1dbbf663c37f89e
Your Name <you@example.com>
2021-08-15 10:52:28 +0530

    Welcome Messege

---
+++ LinkedList_Stack_Queue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedList_Stack_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------Welcome to LinedList Stack And Queue Problem-------------------");
+        }
+    }
+}
