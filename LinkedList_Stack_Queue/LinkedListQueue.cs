1281cb378689299dd23c7e2fa54f9c1d3b67d1af
Your Name <you@example.com>
2021-08-15 11:34:36 +0530

    feat:By using Enqueue method added elemnt in Queue

---
+++ LinkedList_Stack_Queue/LinkedListQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList_Stack_Queue
+{
+    class LinkedListQueue
+    {
+        Node head = null;
+     
+
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+                }
+                temp.next = node;
+            }
+
+            Console.WriteLine("{0} inserted into Queue,", node.data);
+
+        }
+
+       
+
+
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+
+
+
+    }
+       
+
+
+}
+
--- LinkedList_Stack_Queue/Program.cs
+++ LinkedList_Stack_Queue/Program.cs
@@ -7,15 +7,15 @@ namespace LinkedList_Stack_Queue
         static void Main(string[] args)
         {
             Console.WriteLine("----------------Welcome to LinedList Stack And Queue Problem-------------------");
-            LinkedListStack list = new LinkedListStack();
-            list.push(70);
-            list.push(30);
-            list.push(56);
-            list.Display();
-            list.peek();
-            list.pop();
-            list.IsEmpty();
-            list.peek();
+            LinkedListQueue list1 = new LinkedListQueue();
+
+            list1.Enqueue(56);
+            list1.Enqueue(30);
+            list1.Enqueue(70);
+            list1.Display();
+
+
+
 
 
 
